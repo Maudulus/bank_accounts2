d3203bf9a8d89079c828e6e4edb8da628209d68e
Modsky1 <maude.jake@gmail.com>
2014-03-06 17:01:40 -0500

    all my files are now restored after being deleted, thanks to dropbox

--- bankaccount.rb
+++ bankaccount.rb
@@ -1,23 +1,43 @@
 class BankAccount
+  attr_accessor :starting_balance, :ending_balance
+  attr_reader :transactions, :name
 
-  def initialize(account,balance)
-    @account = account
+  def initialize(name ,balance, transactions = [])
+    @name = name
     @balance = balance
-    @
+    @transactions = transactions
   end
-  #make an instance variable that contains an array of all the bank transactions for each account
 
   def starting_balance
-       @balance
+    @balance
   end
 
   def ending_balance
-    @balance-
-    #total changes to accounts
+    running_total = @balance
+    @transactions.each do |trans|
+      running_total += trans.amount
+    end
+    running_total
+  end
 
+
+  def add_transaction(trans)
+    @transactions << trans
   end
 
   def summary
-
+    puts "==== #{name} ===="
+    puts "Starting Balance: #{@balance}"
+    puts "Ending Balance: #{ending_balance}"
+    puts ""
+    @transactions.each do |summarize|
+      if summarize.amount < 0
+        puts "$#{summarize.amount}   DEBIT   #{summarize.date} - #{summarize.description}"
+      else
+        puts "$#{summarize.amount}   CREDIT   #{summarize.date} - #{summarize.description}"
+      end
+    end
+    puts ""
   end
+
 end
--- finances.rb
+++ finances.rb
@@ -4,55 +4,35 @@ require 'csv'
 require 'pry'
 
 credits_and_debits = []
-bank_accounts = []
+
+accounts = []
+CSV.foreach('balances.csv', headers: true) do |row|
+  accounts << BankAccount.new(row["Account"],row["Balance"].to_f)
+end
 
 CSV.foreach('bank_data.csv', headers: true) do |row|
   credits_and_debits << BankTransaction.new(row['Date'],row['Amount'].to_f,row['Description'],row['Account'])
 end
-# credits_and_debits #.each do |row|
-#   row['Amount'].debit?
 
-CSV.foreach('balances.csv', headers: true) do |row|
-  bank_accounts << BankAccount.new(row["Account"],row["Balance"].to_f)
-end
-
-#put into its respective class def give_summary(credits_and_debits)
-#Do Stuff Different
-
-
-#put into its respective
-def end_of_balance(bank_accounts, credits_and_debits)
-  if section.account == "Purchasing Account"#if the transaction came from the Purchasing Account
-#     bank_accounts.balance = bank_accounts.balance - section.amount
-#   else #if the transaction came from the Business Checking
-#     bank_accounts.balance = bank_accounts.balance - section.amount
-end
-end
-
-#
-def call_cred_deb(array)
-  array.each do |transaction|
-   puts transaction.summary
+def which_account(credits_and_debits,accounts)
+  credits_and_debits.each do |transaction|
+    accounts.each do |account|
+      if account.name == transaction.account
+        account.add_transaction(transaction)
+        break
+      end
+    end
   end
 end
-binding.pry
-bank_accounts[0].starting_balance
-call_cred_deb(credits_and_debits)
-#give_summary(credits_and_debits)
-#starting_balance(bank_accounts)
-  #bank_accounts.each.starting_balance
-# end
-# credits_and_debits.each do |section|
-# puts bank_accounts.balance
-#   if section.account == "Purchasing Account"#if the transaction came from the Purchasing Account
-#     bank_accounts.balance = bank_accounts.balance - section.amount
-#   else #if the transaction came from the Business Checking
-#     bank_accounts.balance = bank_accounts.balance - section.amount
-#   end
-# end
-
-# parked_cars.each do |car|
-#   puts "#{car.owner} owns the #{car.color} #{car.cylinders}-cylinder"
-# end
+which_account(credits_and_debits, accounts)
 
 
+def show_me_the_transactions(the_bank )
+  the_bank.each do |act|
+    act.ending_balance(transactions)
+  end
+end
+
+
+accounts[0].summary
+accounts[1].summary
